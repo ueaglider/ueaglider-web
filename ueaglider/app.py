a4d661f231d082dc554b2097360c15ad62491054
Callum Rollo <c.rollo@outlook.com>
2021-01-06 09:54:27 +0000

    using common template layout

--- ueaglider/app.py
+++ ueaglider/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 @response(template_file='home/index.html')
 def index():
+    """Index main page of the site
+
+    :return: dictionary containing title for the main content h1
+    """
     return {'title': 'My awesome title'}
 
 
