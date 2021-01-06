2822e5a8c9ea63ea5f7c512a28299750261296ef
Callum Rollo <c.rollo@outlook.com>
2021-01-06 09:34:51 +0000

    first template

--- ueaglider/app.py
+++ ueaglider/app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from ueaglider.infrastructure.view_modifiers import response
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@response(template_file='home/index.html')
+def index():
+    return {'title': 'My awesome title'}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
