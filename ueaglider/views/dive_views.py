b08622dd26634a81f08b7e78aebbb91ee5293f04
Callum Rollo <c.rollo@outlook.com>
2021-01-13 21:04:47 +0000

    less hacky hotfix

--- ueaglider/views/dive_views.py
+++ ueaglider/views/dive_views.py
@@ -1,13 +1,12 @@
 import flask
 from ueaglider.infrastructure.view_modifiers import response
-import glob
 import os
 import sys
 from pathlib import Path
 folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, folder)
 blueprint = flask.Blueprint('dives', __name__, template_folder='templates')
-
+print(folder)
 
 @blueprint.route('/mission<int:mission_id>/glider<int:glider_num>/science')
 @response(template_file='missions/dive.html')
@@ -18,14 +17,16 @@ def science(mission_id: int, glider_num: int):
     dive_plots: list of paths to images associated with the dive
     """
     folder_path = Path(folder)
+    # Find the absolute path to the figures
     path_add = 'static/img/dives/Mission' + str(mission_id) + '/' + str(glider_num) + '/Science'
     dive_path = folder_path / path_add
     figure_paths = sorted(dive_path.glob('*'))
-    figure_strings = []
     dive_plot_paths = []
     for path in figure_paths:
-        figure_strings.append(str(path))
-        dive_plot_paths.append(str(path)[30:])
+        path_str = str(path)
+        # Cut the path to each figure so it starts from 'static' directory in app's home directory
+        rel_path = path_str[path_str.find('/static'):]
+        dive_plot_paths.append(rel_path)
     links_dict = {
         'glider status': "/mission" + str(mission_id) + "/glider" + str(glider_num) + "/status",
         'science': "/mission" + str(mission_id) + "/glider" + str(glider_num) + "/science",
@@ -49,11 +50,11 @@ def status(mission_id: int, glider_num: int):
     path_add = 'static/img/dives/Mission' + str(mission_id) + '/' + str(glider_num) + '/Monitor'
     dive_path = folder_path / path_add
     figure_paths = sorted(dive_path.glob('*'))
-    figure_strings = []
     dive_plot_paths = []
     for path in figure_paths:
-        figure_strings.append(str(path))
-        dive_plot_paths.append(str(path)[30:])
+        path_str = str(path)
+        rel_path = path_str[path_str.find('/static'):]
+        dive_plot_paths.append(rel_path)
     links_dict = {
         'glider status': "/mission" + str(mission_id) + "/glider" + str(glider_num) + "/status",
         'science': "/mission" + str(mission_id) + "/glider" + str(glider_num) + "/science",
@@ -76,11 +77,11 @@ def dive(mission_id: int, glider_num: int, dive_num: int):
     path_add = 'static/img/dives/Mission' + str(mission_id) + '/' + str(glider_num) + '/Dive' + str(dive_num).zfill(4)
     dive_path = folder_path / path_add
     figure_paths = sorted(dive_path.glob('*'))
-    figure_strings = []
     dive_plot_paths = []
     for path in figure_paths:
-        figure_strings.append(str(path))
-        dive_plot_paths.append(str(path)[30:])
+        path_str = str(path)
+        rel_path = path_str[path_str.find('/static'):]
+        dive_plot_paths.append(rel_path)
     links_dict = {
         'prev dive': "/mission" + str(mission_id) + "/glider" + str(glider_num) + "/dive" + str(dive_num - 1),
         'glider status': "/mission" + str(mission_id) + "/glider" + str(glider_num) + "/status",
@@ -89,7 +90,6 @@ def dive(mission_id: int, glider_num: int, dive_num: int):
         'next dive': "/mission" + str(mission_id) + "/glider" + str(glider_num) + "/dive" + str(dive_num + 1),
     }
     return {
-        'path': figure_strings,
         'dive_plots': dive_plot_paths,
         'links_dict': links_dict
     }
