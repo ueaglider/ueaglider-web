a3e1e89ab09d4fa9966601968347b5b178f78e8d
Callum Rollo <c.rollo@outlook.com>
2021-01-13 17:30:44 +0000

    clear unused imports and debug

--- ueaglider/app.py
+++ ueaglider/app.py
@@ -4,20 +4,15 @@ import os
 folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, folder)
 from ueaglider.data.db_session import global_init, create_session
-from ueaglider.data.gliders import Gliders
-from ueaglider.infrastructure.view_modifiers import response
 
 app = Flask(__name__)
-
-# Echo SQL queries for debugging
-app.config['SQLALCHEMY_ECHO'] = True
 
 def main():
     # Collect all the page routes and instructions
     register_blueprints()
     # Initialise the database
     global_init('seaglider')
-    app.run(debug=True)
+    app.run(debug=False)
 
 def register_blueprints():
     from ueaglider.views import home_views
