f5a044753239fb1ded53b0e23208f57b64d7d43f
Callum Rollo <c.rollo@outlook.com>
2021-01-26 17:53:48 +0000

    basic home page tests

---
+++ tests/_all_tests.py
@@ -0,0 +1,13 @@
+import sys
+import os
+container_folder = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..'
+))
+sys.path.insert(0, container_folder)
+
+
+
+# noinspection PyUnresolvedReferences
+from sitemap_tests import *
+# noinspection PyUnresolvedReferences
+from home_tests import *
---
+++ tests/home_tests.py
@@ -0,0 +1,21 @@
+from flask import Response
+
+from tests.test_client import client, flask_app
+from ueaglider.views import home_views
+
+
+def test_int_homepage(client):
+    # Check that the home page returns a 200 and has an expected string in it
+    r: Response = client.get('/')
+    assert r.status_code == 200
+    assert b'UEA glider group' in r.data
+
+
+def test_homepage_directly():
+    # Check that the mission list has at least one item in it
+    with flask_app.test_request_context(path='/'):
+        r: Response = home_views.index()
+
+    assert r.status_code == 200
+    # noinspection PyUnresolvedReferences
+    assert len(r.model.get('mission_list')) > 0
