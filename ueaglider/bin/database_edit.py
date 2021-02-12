20c83adc7bcb1b17a5f7c36e76e70e5d1bb97ca6
Callum Rollo <c.rollo@outlook.com>
2021-02-12 17:44:35 +0000

    functioning dive add

--- ueaglider/bin/database_edit.py
+++ ueaglider/bin/database_edit.py
@@ -1,19 +1,38 @@
-from ueaglider.data.db_session import create_session
-from ueaglider.data.db_classes import Dives
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
+from ueaglider.data.db_classes import Dives, Gliders
 
+import json
+import os
+folder = os.path.abspath(os.path.dirname(__file__))
+# Store credentials in a external file that is never added to git or shared over insecure channels
+with open(folder+'/ueaglider/secrets.txt') as json_file:
+    secrets = json.load(json_file)
 
-def add_dive(mission_num):
-    dive = Dives
+#from ueaglider.data.db_session import create_session
+
+conn_str = 'mysql+pymysql://' + secrets['sql_user'] + ':' + secrets['sql_pwd'] + '@' + secrets['remote_string'] \
+           + '/' + secrets['db_name']
+
+# Can switch echo to True for debug, SQL actions print out to terminal
+engine = sa.create_engine(conn_str, echo=False)
+
+__factory = orm.sessionmaker(bind=engine)
+session: Session = __factory()
+session.expire_on_commit = False
+
+def add_dive(mission_num, glider_num, dive_no, lon, lat):
+    dive = Dives()
+    glider = session.query(Gliders).filter(Gliders.Number == int(glider_num)).first()
     dive.MissionID = mission_num
-    dive.GliderID
-
-
-def edit_waypoint_info(waypoint_id, info_txt):
-    if not waypoint_id:
-        return None
-    session = create_session()
-    waypoint = session.query(Pins).filter(Pins.WaypointsID == waypoint_id).first()
-    waypoint.Info = info_txt
+    dive.GliderID = glider.GliderID
+    dive.Longitude = lon
+    dive.Latitude = lat
+    dive.DiveNo = dive_no
+    session.add(dive)
     session.commit()
     session.close()
-    retur
+
+
+add_dive(60,637,1,30,30)
