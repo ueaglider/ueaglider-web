b6ebda822dc3c9b59ce2dcc4d131cda633d003eb
Callum Rollo <c.rollo@outlook.com>
2021-02-19 10:25:29 +0000

    adapted shell scritp to basestation

--- ueaglider/bin/add_dive.sh
+++ ueaglider/bin/add_dive.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/media/callum/storage/Documents/basestation-migration/ueaglider-web/venv/bin/python /media/callum/storage/Documents/basestation-migration/ueaglider-web/ueaglider/bin/database_edit.py  $(whoami)
+/home/pilot/Python/ueaglider-web/venv/bin/python /home/pilot/Python/ueaglider-web/ueaglider/bin/database_edit.py  $(whoami)
+
