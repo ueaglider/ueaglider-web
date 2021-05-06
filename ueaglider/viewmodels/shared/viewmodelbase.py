c9877e1c743b67bc1a87192fdf22025db30e425a
Callum Rollo <c.rollo@outlook.com>
2021-05-06 18:18:24 +0100

    implemented CSP for all scripts with nonce

--- ueaglider/viewmodels/shared/viewmodelbase.py
+++ ueaglider/viewmodels/shared/viewmodelbase.py
@@ -1,5 +1,6 @@
 from typing import Optional
-
+import datetime
+import uuid
 import flask
 from flask import Request
 
@@ -10,7 +11,8 @@ class ViewModelBase:
     def __init__(self):
         self.request: Request = flask.request
         self.request_dict = request_dict.create('')
-
+        self.year = datetime.datetime.now().year
+        self.nonce = uuid.uuid4().hex
         self.error: Optional[str] = None
         self.message: Optional[str] = None
         self.user_id: Optional[int] = cookie_auth.get_user_id_via_auth_cookie(self.request)
