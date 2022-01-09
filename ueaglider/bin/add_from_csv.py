7b687ca61341291ce04596fa9de3eba15c35faa0
Callum Rollo <c.rollo@outlook.com>
2022-01-09 16:14:14 -0300

    passing data to and from csv files

---
+++ ueaglider/bin/add_from_csv.py
@@ -0,0 +1,64 @@
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+import sys
+import datetime
+import json
+import os
+import pandas as pd
+
+folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.insert(0, folder)
+from ueaglider.data.db_classes import Dives, Targets, ArgosLocations, Pins
+with open(folder + '/ueaglider/secrets.txt') as json_file:
+    secrets = json.load(json_file)
+
+conn_str = 'mysql+pymysql://' + secrets['sql_user'] + ':' + secrets['sql_pwd'] + '@' + secrets['remote_string'] \
+           + '/' + secrets['db_name']
+
+# Can switch echo to True for debug, SQL actions print out to terminal
+engine = sqlalchemy.create_engine(conn_str, echo=False)
+Session = sessionmaker(bind=engine)
+
+
+def add_dives(dive_csv):
+    df = pd.read_csv(dive_csv)
+    session = Session()
+    for i, row in df.iterrows():
+        dive = Dives()
+        for key, val in row.items():
+            setattr(dive, key, val)
+        dive_exists = session.query(Dives) \
+            .filter(Dives.GliderID == int(row['GliderID'])) \
+            .filter(Dives.DiveNo == row['DiveNo']) \
+            .filter(Dives.MissionID == row['MissionID']) \
+            .first()
+        # stop if dive already exists
+        if dive_exists:
+            return
+        session.add(dive)
+    session.commit()
+    session.close()
+
+
+def add_targets(target_csv):
+    df = pd.read_csv(target_csv)
+    session = Session()
+    for i, row in df.iterrows():
+        tgt = Targets()
+        for key, val in row.items():
+            setattr(tgt, key, val)
+        tgt_exists = session.query(Targets) \
+            .filter(Targets.Name == int(row['Name'])) \
+            .filter(Targets.MissionID == row['MissionID']) \
+            .first()
+        # stop if dive already exists
+        if tgt_exists:
+            return
+        session.add(tgt)
+    session.commit()
+    session.close()
+
+
+if __name__ == '__main__':
+    add_dives(f'{folder}/output/glider_locs.csv')
+    add_targets(f'{folder}/output/targets.csv')
--- ueaglider/bin/email_locs.py
+++ ueaglider/bin/email_locs.py
@@ -4,12 +4,11 @@ import sys
 import datetime
 import json
 import os
-import numpy as np
 import pandas as pd
 
 folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 sys.path.insert(0, folder)
-from ueaglider.data.db_classes import Dives
+from ueaglider.data.db_classes import Dives, Targets, ArgosLocations, Pins
 with open(folder + '/ueaglider/secrets.txt') as json_file:
     secrets = json.load(json_file)
 
@@ -21,24 +20,45 @@ engine = sqlalchemy.create_engine(conn_str, echo=False)
 Session = sessionmaker(bind=engine)
 
 
-def main():
+def get_dives():
     session = Session()
-    start = datetime.datetime.now() - datetime.timedelta(hours=36)
-    dives = session.query(Dives)\
+    start = datetime.datetime.now() - datetime.timedelta(hours=3006)
+    rows = session.query(Dives)\
         .filter(Dives.ReceivedDate > start)\
         .all()
-    glider, divenum, times, lon, lat = [], [], [], [], []
     session.close()
-    for dive in dives:
-        glider.append(dive.GliderID)
-        divenum.append(dive.DiveNo)
-        times.append(dive.ReceivedDate)
-        lat.append(dive.Latitude)
-        lon.append(dive.Longitude)
-    df = pd.DataFrame({"glider": glider, "dive": divenum, "datetime":times, "lon": lon, "lat": lat})
-    df["lon"] = np.round(df.lon.values, 4)
-    df["lat"] = np.round(df.lat.values, 4)
-    df.to_csv('/apps/glider_locs.csv', index=False)
+    if not rows:
+        return
+    df = rows_to_df(rows)
+    df.to_csv(folder + '/output/glider_locs.csv', index=False)
+
+
+def rows_to_df(rows):
+    inst_dict = rows[0].__dict__
+    inst_dict.pop('_sa_instance_state')
+    keys = inst_dict.keys()
+    data_dict = {}
+    for key in keys:
+        data = []
+        for row in rows:
+            data.append(getattr(row, key))
+        data_dict[key] = data
+    df = pd.DataFrame(data_dict)
+    return df
+
+
+def get_tgts():
+    session = Session()
+    rows = session.query(Targets)\
+        .filter(Targets.MissionID == 64)\
+        .all()
+    session.close()
+    if not rows:
+        return
+    df = rows_to_df(rows)
+    df.to_csv(f'{folder}/output/targets.csv', index=False)
+
 
 if __name__ == '__main__':
-    main()
+    get_tgts()
+    get_dives()
