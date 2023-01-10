af9ea3c29c9471f14ebbbc19ff8242e1e4f4bb04
ESiddle <bethsiddle@hotmail.co.uk>
2023-01-10 12:24:38 +0000

    add logging to add_dive.sh

--- ueaglider/bin/add_dive.sh
+++ ueaglider/bin/add_dive.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-/home/pilot/Python/ueaglider-web/venv/bin/python /home/pilot/Python/ueaglider-web/ueaglider/bin/database_edit.py  $(whoami)
-
+/home/pilot/Python/ueaglider-web/venv/bin/python /home/pilot/Python/ueaglider-web/ueaglider/bin/database_edit.py  $(whoami) >> add_dive.log 2>&1
